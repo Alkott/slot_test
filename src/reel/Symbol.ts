7e48068083eeff26165d074b286c76f194cdc93b
alkott <alkott@hotmail.com>
2026-05-16 11:54:39 +0200

    fix: rename private label field to labelText to avoid Container.label type conflict

--- src/reel/Symbol.ts
+++ src/reel/Symbol.ts
@@ -3,23 +3,23 @@ import { getSymbolData, randomSymbolIndex } from './symbolData'
 
 export class Symbol extends Container {
   private bg = new Graphics()
-  private label = new Text({
+  private labelText = new Text({
     text: '1',
     style: { fill: 0xffffff, fontSize: 32, fontWeight: 'bold', align: 'center' },
   })
 
   constructor() {
     super()
-    this.label.anchor.set(0.5)
-    this.addChild(this.bg, this.label)
+    this.labelText.anchor.set(0.5)
+    this.addChild(this.bg, this.labelText)
   }
 
   randomize(width: number, height: number): void {
     const data = getSymbolData(randomSymbolIndex())
     this.bg.clear()
     this.bg.rect(1, 1, width - 2, height - 2).fill(data.color)
-    this.label.text = data.label
-    this.label.x = width / 2
-    this.label.y = height / 2
+    this.labelText.text = data.label
+    this.labelText.x = width / 2
+    this.labelText.y = height / 2
   }
 }
